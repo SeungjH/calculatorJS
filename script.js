a18520dbd644c314c3f3614d9f3705364affb0da
Brian Han <75606685+SeungjH@users.noreply.github.com>
2024-05-22 13:50:08 +0000

    working on advanced calculator functions

--- script.js
+++ script.js
@@ -22,8 +22,20 @@ function clearDisplay() {
 
 //if operator is clicked, highlight selected operator, clear display and store value
 //if equals is clicked, calculate and display result
-function onClickOperator{
-  
+function onClick(){
+  //handling displaying operators in blank screen
+  if(display.value === ("+" || "-" || "*" || "/")){
+    clearDisplay();
+  }
+  //handling displaying operators in a row
+  //it also should change the operator into the following one
+  if(display.value.length > 0 && display.value.slice(display.value.length-1, display.value.length) === ("-" || "+" || "*" || "/") && display.value.slice(display.value.length-2, display.value.length-1) === ("-" || "+" || "*" || "/")){
+    let temp = display.value.slice(display.value.length-1, display.value.length);
+    display.value = display.value.slice(0, display.value.length-2);
+    display.value += temp;
+    const button = document.getElementById(temp);
+    button.addEventListener('click', handleClick);
+  }
 }
 //Add event listeners to the buttons
 
