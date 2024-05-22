7ac5aeb0bfed36fac961416f9a6dc8fc8d3f05f4
Brian Han <75606685+SeungjH@users.noreply.github.com>
2024-05-22 14:37:46 +0000

    working on screen resetting

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("display");
 
+var operatorSet = new Set(["+", "-", "/", "*"]);
+
 //Three functions to create: 
 //appendToDisplay, calcualte, clearDisplay
 
@@ -20,16 +22,21 @@ function clearDisplay() {
   display.value = "";
 }
 
+function handleClick(){
+  //does nothing
+}
+
 //if operator is clicked, highlight selected operator, clear display and store value
 //if equals is clicked, calculate and display result
 function onClick(){
   //handling displaying operators in blank screen
-  if(display.value === ("+" || "-" || "*" || "/")){
+  if(display.value.length > 0 && operatorSet.has(display.value)){
     clearDisplay();
   }
   //handling displaying operators in a row
   //it also should change the operator into the following one
-  if(display.value.length > 0 && display.value.slice(display.value.length-1, display.value.length) === ("-" || "+" || "*" || "/") && display.value.slice(display.value.length-2, display.value.length-1) === ("-" || "+" || "*" || "/")){
+  if(display.value.length > 0 && operatorSet.has(display.value.slice(display.value.length-1, display.value.length)) && operatorSet.has(display.value.slice(display.value.length-2, display.value.length-1))){
+    
     let temp = display.value.slice(display.value.length-1, display.value.length);
     display.value = display.value.slice(0, display.value.length-2);
     display.value += temp;
@@ -37,6 +44,10 @@ function onClick(){
     button.addEventListener('click', handleClick);
   }
 }
-//Add event listeners to the buttons
+//resets the display if the previous calcualtion is finished and the user starts another calculation
+function resetScreen(){
+  
+  //display.addEventListener("input", ());
+}
 
 const buttons = document.querySelectorAll("button");
